fc71483f70469f4d86bb87d19c73c5fa5d35c744
Prathyusha Maram <108296399+Prathyusha-Maram@users.noreply.github.com>
2023-05-10 22:15:49 -0500

    admin ui

--- nwconf2023-fe-development/src/pages/loginPage/LoginTabs/AdminSignUpPage.jsx
+++ nwconf2023-fe-development/src/pages/loginPage/LoginTabs/AdminSignUpPage.jsx
@@ -91,57 +91,83 @@ const AdminSignUpPage = () => {
     }
   }
   return (
-    <div className="login-page-container">
-      <div className="login-con">
-        <h2>Chariman Register</h2>
+    <div className="login-container">
+      <div className="login-con" style={{ margin: "40px 0" }}>
+        <h2 style={{textAlign:"center"}}>Chariman Register</h2>
         <div className="login-input-con new">
+		    <form onSubmit={AdminSignup}style={{ display:"flex", flexDirection:"column" }}>
+          <div className="nameContainer" style={{ display: 'flex', width:"700px" }}>
+          <div style={{flex: 1}}>
           <label htmlFor="firstName">First Name</label>
           <input
             type="text" className={`form-control ${firstNameError ? 'is-invalid' : ''}`} id="firstName"
             placeholder="First Name"
             onChange={(e) => setFirstName(e.target.value)}
+            style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '5px' , width: "100%" }}
           />
           {firstNameError && <div className="invalid-feedback">{firstNameError}</div>}
+          </div>
+          <div style={{ marginLeft: "20px", flex:1 }}>
           <label htmlFor="lastName">Last Name</label>
           <input
             type="text" className={`form-control ${lastNameError ? 'is-invalid' : ''}`} id="lastName"
             placeholder="Last Name"
             onChange={(e) => setLastName(e.target.value)}
+            style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '5px' , width: "100%" }}
           />
           {lastNameError && <div className="invalid-feedback">{lastNameError}</div>}
+          </div>
+          </div>
+          <div className="emailContainer" style={{ display: "flex", width:"700px"}}>
+          <div style={{flex: 1}}>
           <label htmlFor="email">Email Address</label>
           <input
             type="email" className={`form-control ${emailError ? 'is-invalid' : ''}`} id="email"
             placeholder="Email"
             onChange={(e) => setSignUpReviewerEmail(e.target.value)}
+            style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '5px' , width: "100%" }}
           />
           {emailError && <div className="invalid-feedback">{emailError}</div>}
+          </div>
+          <div style={{ marginLeft: "20px", flex:1 }}>
           <label htmlFor="confirmEmail">Confirm Email Address</label>
           <input
             type="email" className={`form-control ${confirmEmailError ? 'is-invalid' : ''}`} id="confirmEmail"
             placeholder="Confirm Email"
             onChange={(e) => setConfirmSignUpReviewerEmail(e.target.value)}
+            style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '5px' , width: "100%" }}
           />
           {confirmEmailError && <div className="invalid-feedback">{confirmEmailError}</div>}
+          </div>
+          </div>
+          <div className="emailContainer" style={{ display: "flex", width:"700px"}}>
+          <div style={{flex: 1}}>
           <label htmlFor="password">Passowrd</label>
           <input
             type="password" className={`form-control ${passwordError ? 'is-invalid' : ''}`} id="password"
             placeholder="Password"
             onChange={(e) => setSignUpReviewerPassword(e.target.value)}
+            style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '5px' , width: "100%" }}
           />
           {passwordError && <div className="invalid-feedback">{passwordError}</div>}
+          </div>
+          <div style={{ marginLeft: "20px", flex:1 }}>
           <label htmlFor="confrimPassword">Confirm Passowrd</label>
           <input
             type="password" className={`form-control ${confirmPasswordError ? 'is-invalid' : ''}`} id="confirmpassword"
             placeholder="Confirm Password"
             onChange={(e) => setConfirmSignUpReviewerPassword(e.target.value)}
+            style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '5px' , width: "100%" }}
           />
           {confirmPasswordError && <div className="invalid-feedback">{confirmPasswordError}</div>}
+          </div>
+          </div>
           <label htmlFor="areaOfIntrest">Area Of Intrest</label>
           <input
             type="text" className={`form-control ${interestsError ? 'is-invalid' : ''}`} id="interests"
             placeholder="Area Of Intrest"
             onChange={(e) => setAreaOfInterest(e.target.value)}
+            style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '5px' , width: "100%" }}
           />
           {reivewerSignUpTost ? (
             <strong style={{ color: "green" }}>Sign up Sucessfull</strong>
@@ -153,7 +179,10 @@ const AdminSignUpPage = () => {
           ) : (
             ""
           )}
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <button onClick={AdminSignup}>Sign UP</button>
+		  </div>
+          </form>
           <p> Already have an account? <NavLink to="/committee/login">Login Here</NavLink> </p>
         </div>
       </div>
