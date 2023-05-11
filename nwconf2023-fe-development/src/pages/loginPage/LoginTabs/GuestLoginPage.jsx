f077015e2a5a5a7eeef48ceb1c6df26b3e0e8435
Prathyusha Maram <108296399+Prathyusha-Maram@users.noreply.github.com>
2023-05-11 12:35:58 -0500

    guest ui

--- nwconf2023-fe-development/src/pages/loginPage/LoginTabs/GuestLoginPage.jsx
+++ nwconf2023-fe-development/src/pages/loginPage/LoginTabs/GuestLoginPage.jsx
@@ -19,16 +19,16 @@ const GuestLoginPage = () => {
     }
 
     return (
-        <div className="login-page-container">
-            <div className="login-con">
-                <h2>Guest Login</h2>
-                <div className="guestContainer" style={{ height: "100%", padding: "4rem 0" }}>
+        <div className="login-container" style={{background:"../images/bg6.png"}}>
+            <div className="login-con" style={{textAlign:"center", marginTop:"40px"}}>
+                <h2 >Guest Login</h2>
+                <div className="guestContainer" style={{ height: "100%", padding: "4rem 0"}}>
                     <div className="guestLogin" style={{ width: "35rem" }}>
                         <h3 style={{ color: "#006747", marginBottom: "10px" }}>Make your payment</h3>
                         <p>In order to attend this conference, you are required to make a payment in order to recieve your pass on entering the conference</p>
                         <div className="pay" style={{ display: "flex", justifyContent: "center" }}>
-                            <h5 style={{ color: "#000", marginBottom: "10px" }}>Amount : </h5>
-                            <h2 style={{ color: "#006747", marginBottom: "10px" }}>  $50.00</h2>
+                            <h3 style={{ color: "#000", marginBottom: "10px" }}>Amount : </h3>
+                            <h3 style={{ color: "#006747", marginBottom: "10px" }}>  $50.00</h3>
                         </div>
                         <p style={{ marginTop: "20px" }}>Payment options</p>
                         <PayPalButton amount={50} onSuccess={(details, data) => handleSuccessPayment(details)} />
