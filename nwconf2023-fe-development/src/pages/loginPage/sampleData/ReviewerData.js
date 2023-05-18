f6257b4464b583a275b1f46ad7f65fe14206e0ee
Prathyusha Maram <108296399+Prathyusha-Maram@users.noreply.github.com>
2023-05-17 22:01:54 -0500

    backEnd branch created

--- nwconf2023-fe-development/src/pages/loginPage/sampleData/ReviewerData.js
+++
@@ -1,8 +0,0 @@
-export const ReviewerDetail = [
-    { id: 1, userName: "Ebin", email: "reviewer1@gmail.com" },
-    { id: 2, userName: "Surya", email: "reviewer2@gmail.com" },
-    { id: 3, userName: "Sai", email: "reviewer3@gmail.com" },
-    { id: 4, userName: "Naveen", email: "reviewer4@gmail.com" },
-    { id: 5, userName: "Shiju", email: "reviewer5@gmail.com" },
-
-]
